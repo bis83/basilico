df58126c82d5a7c02ded2c7a74af09d1f09486ef
bis <bis83gb@gmail.com>
2022-09-17 10:10:41 +0900

    rename view.draw

--- pkg/data/type_pack_view.go
+++ pkg/data/type_pack_view.go
@@ -7,14 +7,20 @@ import (
 )
 
 type View struct {
-	SkyBox int   `json:"skybox"`
 	Draw3D bool  `json:"draw3d"`
+	Draw   []int `json:"draw"`
 	Com    []int `json:"com"`
 }
 
 func (p *View) Set(prj *project.Project, view *project.View) error {
 	p.Draw3D = view.Draw3D
-	p.SkyBox = prj.FindDraw(view.SkyBox)
+	for _, v := range view.Draw {
+		i := prj.FindDraw(v)
+		if i < 0 {
+			return fmt.Errorf("Draw Not Found: %s", v)
+		}
+		p.Draw = append(p.Draw, i)
+	}
 	for _, v := range view.Com {
 		i := prj.FindCom(v)
 		if i < 0 {
--- pkg/project/type_page.go
+++ pkg/project/type_page.go
@@ -45,7 +45,7 @@ type Draw struct {
 type View struct {
 	Name   string   `toml:"name"`
 	Draw3D bool     `toml:"draw3d"`
-	SkyBox string   `toml:"skybox"`
+	Draw   []string `toml:"draw"`
 	Com    []string `toml:"com"`
 }
 
