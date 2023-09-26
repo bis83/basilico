e7e0ec96c06a26d09858374b2b952444f97f4a93
bis <bis83gb@gmail.com>
2023-09-27 08:52:46 +0900

    fovx

--- pkg/basil3d/web/js/gpu_buffer.js
+++ pkg/basil3d/web/js/gpu_buffer.js
@@ -5,7 +5,8 @@ const basil3d_gpu_upload_view_input = (gpu, device, canvas, view) => {
   const buf = new Float32Array(52);
 
   const aspect = canvas.width / canvas.height;
-  const fovy = deg2rad(camera.fov);
+  const fovx = deg2rad(camera.fov);
+  const fovy = 2 * Math.atan(Math.tan(fovx / 2) / aspect);
   const dir = vec3dir(camera.ha, camera.va);
   const eye = camera.eye;
   const at = vec3add(eye, dir);
